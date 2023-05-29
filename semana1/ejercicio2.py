32738f048baf9a967588a9990849a0e0435ea395
Walter Fontoura <walterfontoura82@gmail.com>
2023-05-28 21:01:25 -0300

    ejercicio1 y 2 + solucion

---
+++ semana1/ejercicio2.py
@@ -0,0 +1,7 @@
+# 2-Escribe un programa que solicite al usuario su nombre y luego imprima un
+# mensaje de bienvenida.
+
+nombre=""
+
+nombre=input("Ingrese su nombre: ")
+print("Bienvenido a la miseria ", nombre)
