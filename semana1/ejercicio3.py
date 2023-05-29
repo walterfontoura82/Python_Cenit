0e2d48dae2eee73118eacdafc4c11a41bb333045
Walter Fontoura <walterfontoura82@gmail.com>
2023-05-28 22:22:44 -0300

    Agrego Ejercicio 3 y 4 mas solucion

---
+++ semana1/ejercicio3.py
@@ -0,0 +1,5 @@
+# 3-Crea un programa que pida al usuario su edad y lo imprima en pantalla.
+
+edad=0
+edad=input("Ingrese su edad: ")
+print("la esdad es: ", edad )
---
+++ semana1/ejercicio4.py
@@ -0,0 +1,10 @@
+# 4-Crea un programa que calcule la suma de dos números y lo imprima en
+# pantalla.
+
+num1=0
+num2=0
+
+num1=int(input("ingrese el primer numero: "))
+num2=int(input("ingrese el segundo numero: 3"))
+
+print("La suma de los dos numeros es: ", num2+num1)
