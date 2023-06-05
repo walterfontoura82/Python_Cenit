68c5ce2c10456f7806dde7af51e9f7afb1041c06
Walter Fontoura <walterfontoura82@gmail.com>
2023-06-05 20:35:31 -0300

    Eje 1 semana2 mas carpeta para separa tipo de ejercicio

---
+++ Semana2/ejercicio1.py
@@ -0,0 +1,11 @@
+# 1-Escribir un programa que pida al usuario su edad y muestre por pantalla si es
+# mayor de edad (18 años o más) o no.
+
+edad=0
+edad=int(input("Ingrese su edad: "))
+print ("------------------")
+if edad >18:
+    print(" Es mayor de edad")
+
+else:
+    print ("Es menor de edad")
