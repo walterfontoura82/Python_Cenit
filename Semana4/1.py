1235aaa594445c2a33eeb9f9581bc30a2a2b06c4
Wally Fontoura <walterfontoura82@gmail.com>
2024-08-25 17:55:39 -0300

    add Semana4

---
+++ Semana4/1.py
@@ -0,0 +1,13 @@
+'''
+1-Crea una función llamada suma que tome dos números como parámetros y
+devuelva la suma de ellos.
+
+'''
+
+def suma(a,b):
+    return a+b
+
+#main
+a= int(input("Ingrese el numer a: "))
+b= int(input("Ingrese el numer b: "))
+print(suma(a,b))
