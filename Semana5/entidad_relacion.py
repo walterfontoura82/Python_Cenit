8d60f95c83bdf7b41b699a675a20a9068e96b5da
Wally Fontoura <walterfontoura82@gmail.com>
2025-01-05 09:42:42 -0300

    Add file at Semana5

---
+++ Semana5/entidad_relacion.py
@@ -0,0 +1,8 @@
+'''
+Ejercicio 1:
+Diseña un modelo entidad-relación para una tienda de música. La tienda tiene
+diferentes discos de música, cada uno perteneciente a un género específico. Cada
+disco tiene un título, artista, año de lanzamiento y precio. Los clientes pueden
+realizar compras y cada compra puede contener varios discos.
+
+'''
